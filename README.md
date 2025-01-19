6089499eaa99781d6ac2a8bbb5357f4b497f12f4
mxvwr990 <wr37981@gmail.com>
2025-01-19 16:09:58 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# wahyu
+# brosur-pkk
+ 
---
+++ index.php
@@ -0,0 +1,380 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>My Favorit Person</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/fanny.jpg" rel="icon">
+  <link href="assets/img/fanny.jpg" rel="fanny">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Arsha
+  * Updated: Jul 27 2023 with Bootstrap v5.3.1
+  * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html">Selamat Datang</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">About You</a></li>
+          <li><a class="nav-link scrollto" href="#services">Fanny</a></li>
+          <li><a class="nav-link scrollto" href="#portfolio">Foto Bersama Fanny</a></li>
+          <li><a class="nav-link scrollto" href="#team">Tempat Kita Ngedate</a></li>
+          <li><a class="nav-link scrollto" href="#pricing">Kata-kata Dari Kak Gem</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Instagram Kita</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+          <h1>All About You</h1>
+          <h2>Fan lu tau ga si?</h2>
+          <div class="d-flex justify-content-center justify-content-lg-start">
+            <a href="#about" class="btn-get-started scrollto">Scroll ya biar tau</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Fanny Tasari</h2>
+          <p>Baca deh fann</p>
+        </div>
+
+        <div class="row">
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <h4><a href=""></a></h4>
+              <p>Fan lu tau ga si? kalo lu tuh lucu dan gemessinnnnnn apalagi kalo ketemu sama lu bawaannya tuh happy banngett, btw lu pake pelet kah fan? gw terpikat bgtt anj sama luu, apalagi klo lama ga ketemu sama lu bawaanya tuh kangen muluu</p>
+            </div>
+          </div>
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+              <h4><a href=""></a></h4>
+              <p>Ama mau ngucapin banyak banyak terimakasih buat fanny, semenjak ama deket lagi sama fanny hidup ama jadi lebih bahagai dan jadi lebih berwarna, jujur gw hidup kesepian maknaya gw happy banget klo kita ketemu dan saling bertukar cerita</p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
+            <div class="icon-box">
+              <h4><a href=""></a></h4>
+              <p>Semangat teruss ya fann buat ngejalanin kuliah+jadi anak organisasi, oh iya semangat juga yaa ngerjain lapraknya, kasian bgt baru semester 1 udh di antem sama laprak</p>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+            <div class="icon-box">
+              <h4><a href=""></a></h4>
+              <p>Jika makan odading bisa jadi ironman, terus gw harus makan apa biar bisa jadi ur mine?hehehehehe
+                  gw nungguin lu udh mau 2 tahun fann, tapi gpp deh nunggu lama klo akhirnya bakal sama lu heheheheehehehehehehehehehehhehe
+              </p>
+            </div>
+          </div>
+
+          
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="row">
+          <div class="col-lg-3 cta-btn-container text-center">
+            <a class="cta-btn align-middle" href="#">Back To Dashbord</a>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Cta Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Foto bersama fanny</h2>
+          <p></p>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-img"><img src="assets/fanny.jpg" class="img-fluid" alt=""></div>
+            <div class="portfolio-info">
+              <h4>App 1</h4>
+              <p>App</p>
+              <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
+              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-img"><img src="assets/img/" class="img-fluid" alt=""></div>
+            <div class="portfolio-info">
+              <h4>Web 3</h4>
+              <p>Web</p>
+              <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>
+              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-img"><img src="assets/img/foto1.jpg" class="img-fluid" alt=""></div>
+            <div class="portfolio-info">
+              <h4>Card 2</h4>
+              <p>Card</p>
+              <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 2"><i class="bx bx-plus"></i></a>
+              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-img"><img src="assets/img/foto 5.jpg" class="img-fluid" alt=""></div>
+            <div class="portfolio-info">
+              <h4>Web 2</h4>
+              <p>Web</p>
+              <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Web 2"><i class="bx bx-plus"></i></a>
+              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-img"><img src="assets/img/fanny.jpg" class="img-fluid" alt=""></div>
+            <div class="portfolio-info">
+              <h4>App 3</h4>
+              <p>App</p>
+              <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 3"><i class="bx bx-plus"></i></a>
+              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-img"><img src="assets/img/foto 6.jpg" class="img-fluid" alt=""></div>
+            <div class="portfolio-info">
+              <h4>Card 1</h4>
+              <p>Card</p>
+              <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Card 1"><i class="bx bx-plus"></i></a>
+              <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Tempat kita ngedate</h2>
+          <p></p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="100">
+            <div class="member d-flex align-items-start">
+              <div class="member-info">
+                <h4>Warkop dkt rumah fanny</h4>
+                <span>Wwkwk aneh dah awal-awal malah kesitu</span>
+                <p>Itu tempat first date kitaa, kocak dah first date malah ke warkopp</p>
+                <div class="social">
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="member d-flex align-items-start">
+              <div class="member-info">
+                <h4>Bubur kemang</h4>
+                <span>Ini tempat kedua yang kita datengin</span>
+                <p>Seru deh fann motoran pagi-pagi terus kita nyarap di kemang, gw inget bgt itu pas 13 mei 2023, ga kerasa yaa udh mau 2 tahun</p>
+                <div class="social">
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4" data-aos="zoom-in" data-aos-delay="300">
+            <div class="member d-flex align-items-start">
+              <div class="member-info"> 
+                <h4>Family mart cinere</h4>
+                <span>Tempat ketemu setelah lama ga ketemuu</span>
+                <p>Ini tempat kita ketemu setelah sekian lama kita ga ketemu, di saat itu gw seneng bbt bisa ketemu lu lagii dan kita beli es krim coklat</p>
+                <div class="social">
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4" data-aos="zoom-in" data-aos-delay="400">
+            <div class="member d-flex align-items-start">
+              <div class="member-info">
+                <h4>Richeese,tomoro,gokana,wkts,warjo</h4>
+                <span>Richeese dan tomoro adalah tempat kita ketemu sebelum asing beberapa saat</span>
+                <p>Dan tempat awal ketemu setelah asing adalah gokana, di situ gw seneng bgt bisa ketemu lu lagi dan yg lebih senengnya adalah pas denger kabar klo lu udh putus heheheh, oh iya kita masih ada beberapa tempat yg blm di coba, semoga secepatnya yaa</p>  
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Team Section -->
+
+    <!-- ======= Pricing Section ======= -->
+    <section id="pricing" class="pricing">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Kata-kata dari kak gem</h2>
+          <p>my happiness is you, i don't need to explain how special you are in my life, but i can say that my world smiles when i'm with you. </p>
+        </div>
+
+        <div class="row">
+
+      </div>
+    </section><!-- End Pricing Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Instagram Kita</h2>
+          <p></p>
+        </div>
+
+        <div class="row">
+              </div>
+              <div class="my-3">
+        
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <p>
+              Lucu deh fann klo kita jadian "aduh rama berharap bgt"<br>
+              tapi emng lucu sih klo gw bayanginn<br>
+              lucu dah pokoknya<br><br>
+              <strong>Instagram:</strong> @mxvwr__<br>
+              <strong>Instagram:</strong> @fann.s.a<br>
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#hero">About You</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#services">Fanny</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#portfolio">Foto Bersama Fanny</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#team">Tempat Kita Ngedate</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#pricing">Kata-kata Dari Kak Gem</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#contact">Instagram Kita</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Instagram kita</h4>
+            <div class="social-links mt-3">
+              <a href="https://www.instagram.com/mxvwr__" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="https://www.instagram.com/fann.s.a" class="instagram"><i class="bx bxl-instagram"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div> 
+    </div>
+
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
